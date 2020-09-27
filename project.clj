92632c4d8de8ab648e4fe2cf367ebc2193239739
Wilker Lucio <wilkerlucio@gmail.com>
2020-09-27 13:30:28 -0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wsscode/js-data-interop "0.0.1"
+(defproject com.wsscode/js-data-interop "0.0.2"
   :description "Library to provide alternative tools to interop between Clojurescript immutable data structures and JSON."
   :url "https://github.com/wilkerlucio/js-data-interop"
   :license {:name "MIT"
